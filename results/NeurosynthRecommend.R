5201835f81e61a5dccdc6efb939f8a0aefcc7245
derek <you@example.com>
2017-07-26 14:56:30 -0400

    A static (local) recommendation engine
    works like a charm! needs documentation, though.

--- results/NeurosynthRecommend.R
+++ results/NeurosynthRecommend.R
@@ -2,6 +2,7 @@
 
 require(ExPosition)
 require(data.table)
+require(plotrix)
 
 ##This function is stolen from here: http://lamages.blogspot.com/2013/04/how-to-change-alpha-value-of-colours-in.html
 
@@ -18,7 +19,29 @@ add.alpha <- function(col, alpha=0.65){
 }
 
 
-NeuroRecommend <- function(number.of.neighbors=25,pmid=NA){
+nr.base.plotter <- function(coords, closest.studies, selected.study, studies.colors, x_axis=1, y_axis=2){
+	
+	#dev.new()
+	
+	# base of the plot
+	plot(coords[,c(x_axis,y_axis)],axes=F,type="n", xlab=paste0("Component ",x_axis), ylab=paste0("Component ", y_axis))
+	abline(h=0,lty=2,col="grey80")
+	abline(v=0,lty=2,col="grey80")	
+	
+	## all
+	points(coords[,c(x_axis,y_axis)],col=add.alpha(studies.colors,.2),pch=20,cex=.75)
+
+	## the closest
+	points(coords[closest.studies,c(x_axis,y_axis)],bg= add.alpha(studies.colors[closest.studies],.6),pch=21,cex=.75)
+
+	## the selected study
+	points(coords[selected.study,x_axis],coords[selected.study,y_axis],bg="red",pch=21,cex=1)
+
+	
+}
+
+
+NeurosynthRecommend <- function(number.of.neighbors=50,pmid=NA){
 	
 	studies <- as.matrix(fread('studies_matrix.csv'))
 	
@@ -47,17 +70,16 @@ NeuroRecommend <- function(number.of.neighbors=25,pmid=NA){
 	#studies.info[ names(sort(euc.dists))[1:number.of.neighbors],]
 
 
-		## functionalize and pass in axes.
-	dev.new()
-	plot(studies.coords,axes=F,type="n")
-	abline(h=0,lty=2,col="grey80")
-	abline(v=0,lty=2,col="grey80")	
-	points(studies.coords,col=add.alpha(studies.colors,.2),pch=20,cex=.75)
-	points(studies.coords[these.studies,],bg= add.alpha(studies.colors[these.studies],.6),pch=21,cex=.75)
-	points(studies.coords[pmid,1],studies.coords[pmid,2],bg="red",pch=21,cex=1)
-	
-
+	## functionalize and pass in axes.
+	par(mfrow=c(2,2),oma=c(0,0,2,0))
+	nr.base.plotter(studies.coords, these.studies, this.pmid, studies.colors, x_axis=1, y_axis=2)
+	nr.base.plotter(studies.coords, these.studies, this.pmid, studies.colors, x_axis=1, y_axis=3)	
+	nr.base.plotter(studies.coords, these.studies, this.pmid, studies.colors, x_axis=4, y_axis=5)
+	plot(c(-1,1),c(-1,1),type="n",axes=F,xlab="",ylab="",sub="Top 5")
+	s <- paste("(",these.studies,") ",as.matrix(studies.info[these.studies,c("title")]))
+	addtable2plot("topmiddle",table=as.matrix(gsub('(.{1,50})(\\s|$)', '\\1\n', s[1:5])), cex=.7, display.colnames=F,display.rownames=F, ypad=.01,xpad=.01)
+	title(paste0(pmid,"'s ", number.of.neighbors," nearest semantic neighbors"), outer=T)
+		
 	return(studies.info[these.studies,])
-	
 		
 }
