778e99b7ae35c9c8116ecf87ec5424613e34165f
derek <you@example.com>
2017-12-17 21:46:39 -0500

    More updates
    Very close!

--- results/NeurosynthRecommendR/app.R
+++ results/NeurosynthRecommendR/app.R
@@ -2,6 +2,10 @@ library(shiny)
 load('./data/studies.rda')
 load('./data/words.rda')
 source('./helpers.R')
+
+random.pmid <- sample(studies$pubmed,1)
+random.stem <- sample(words$word,1)
+
 # Define UI ----
 ui <- fluidPage(
   
@@ -11,23 +15,18 @@ ui <- fluidPage(
     sidebarPanel(
       helpText("Choose your own adventure"),
       
-      radioButtons("papers.or.words", h3("Papers or Words"), choices = list("Papers" = 1, "Words" = 2),selected = 1),
-      radioButtons("dot.colors", h3("Color scheme"), choices = list("Cluster colors" = 1, "Grey" = 2),selected = 2),
+      radioButtons("papers.or.words", label = "Study (PMID) or Stem (words)", choices = list("Papers" = 1, "Words" = 2),selected = 1),
+      radioButtons("dot.colors", label = "Color scheme", choices = list("Cluster colors" = 1, "Grey" = 2),selected = 2),
       #sliderInput("Zoom",label = "Range of interest:",min = 0, max = 100, value = c(0, 100)),
-      #selectInput("number.hits", h3("Number of similar papers/words"),choices = list("5" = 1, "10" = 2,"25" = 3, "50" = 4, "100" = 5), selected = 1),
-      sliderInput("zoom.hits", label = "Range of interest:",min = 5, max = 100, value = 5),
-      textInput("pmid.or.word", h3("Text input"), value = "Enter PMID or Word")#,
+      sliderInput("zoom.hits", label = "Number of similar items:",min = 5, max = 100, value = 5),
+      sliderInput("alpha", label = "Color alpha levels:",min = 0, max = 1, value = .5),
+      textInput("pmid.or.word", label = "PMID/Stem input", value = random.pmid)#,
       #selectInput("pmid.or.word_dropdown", h3("Choose paper/word"),choices = list("Choice 1" = 1, "Choice 2" = 2,"Choice 3" = 3), selected = 1),
-      #submitButton("Go!")
       
     ),
     mainPanel(
-      #textOutput("radiobutton1"),
-      #textOutput("radiobutton2"),
-      #textOutput("slider"),
-      #textOutput("inputbox")
-      #sliderInput("Zoom", label = "Range of interest:",min = 0, max = 100, value = c(0, 100))
-      plotOutput("nr.space")
+        ## ugh.
+      plotOutput("nr.space",height="600px",width="600px")
     )
   )
 )
@@ -38,9 +37,9 @@ server <- function(input, output) {
   output$nr.space <- renderPlot(
     {
       if(input$papers.or.words==1){
-        nr.base.studies.plot(studies)
+        studies.plot.panel(studies,input$dot.colors,input$pmid.or.word,input$zoom.hits,input$alpha)
       }else{
-        
+        stems.plot.panel(words,input$dot.colors,input$pmid.or.word,input$zoom.hits,input$alpha)
       }
     }
   )
--- results/NeurosynthRecommendR/helpers.R
+++ results/NeurosynthRecommendR/helpers.R
@@ -1,5 +1,7 @@
 ## Neurosynth RecommendR HelpRs (could I be any lameR? [clearly yes])
 
+  ## This function is stolen from here: http://lamages.blogspot.com/2013/04/how-to-change-alpha-value-of-colours-in.html
+  ## With some slight modifcations.
 add.alpha <- function(col, alpha=0.65){
   apply(
     sapply(col, col2rgb)/255, 
@@ -10,25 +12,46 @@ add.alpha <- function(col, alpha=0.65){
   ) 
 }
 
+nr.base.plot <- function(dat,...){
+  
+  xy.lims <- apply(dat,2,range) * 1.1
+  plot(dat,xlim=xy.lims[,1],ylim=xy.lims[,2],asp=1,...)
+  abline(h=0,lty=2,col="grey80")
+  abline(v=0,lty=2,col="grey80")  
+  
+}
 
-nr.base.studies.plot <- function(studies,col="grey80"){
+studies.plot.panel <- function(studies.dat,color.selector=2,pmid="19789183",similar.set.size=10,alpha=.5){
  
+  if(color.selector==1){
+    col <- studies.dat$color
+  }else{
+    col <- "grey80"
+  }
+  
     ## I need a real base plot for each...
-  par(mfrow=c(2,2))
+  par(mfrow=c(2,2),oma=c(0,0,0,0))
+  nr.base.plot(studies.dat[,c("component_1","component_2")],xlab="Component 1",ylab="Component 2",col=add.alpha(col,alpha=alpha),pch=20,axes=F)
+  nr.base.plot(studies.dat[,c("component_1","component_3")],xlab="Component 1",ylab="Component 3",col=add.alpha(col,alpha=alpha),pch=20,axes=F)
+  nr.base.plot(studies.dat[,c("component_4","component_5")],xlab="Component 4",ylab="Component 5",col=add.alpha(col,alpha=alpha),pch=20,axes=F)
+
   
-  xlims <- ylims <- c(-max(abs(studies[,paste0("component_",1:5)])),max(abs(studies[,paste0("component_",1:5)])))
+}
+
+stems.plot.panel <- function(stems.dat,color.selector=2,stem="",similar.set.size=10,alpha=.5){
   
-  plot(studies[,c("component_1","component_2")],xlab="Component 1",ylab="Component 2",col=add.alpha(col),pch=20,axes=F,xlim=xlims,ylim=ylims)
-  abline(h=0,lty=2,col="grey80")
-  abline(v=0,lty=2,col="grey80")
+  if(color.selector==1){
+    col <- stems.dat$color
+  }else{
+    col <- "grey80"
+  }
   
-  plot(studies[,c("component_1","component_3")],xlab="Component 1",ylab="Component 3",col=add.alpha(col),pch=20,axes=F,xlim=xlims,ylim=ylims)
-  abline(h=0,lty=2,col="grey80")
-  abline(v=0,lty=2,col="grey80")
+  ## I need a real base plot for each...
+  par(mfrow=c(2,2),oma=c(0,0,0,0))
+  nr.base.plot(stems.dat[,c("component_1","component_2")],xlab="Component 1",ylab="Component 2",col=add.alpha(col,alpha=alpha),pch=20,axes=F)
+  nr.base.plot(stems.dat[,c("component_1","component_3")],xlab="Component 1",ylab="Component 3",col=add.alpha(col,alpha=alpha),pch=20,axes=F)
+  nr.base.plot(stems.dat[,c("component_4","component_5")],xlab="Component 4",ylab="Component 5",col=add.alpha(col,alpha=alpha),pch=20,axes=F)
   
-  plot(studies[,c("component_4","component_5")],xlab="Component 4",ylab="Component 5",col=add.alpha(col),pch=20,axes=F,xlim=xlims,ylim=ylims)
-  abline(h=0,lty=2,col="grey80")
-  abline(v=0,lty=2,col="grey80")
   
-}  
+}
   
